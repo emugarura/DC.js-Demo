e0513d7146034a66bf8c32d67940bb265d4532e0
Kyle Tsuyemura <kt.fremont@gmail.com>
2017-05-04 13:48:52 -0700

    fixing files

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var bodyParser =require('body-parser');
+var path = require('path');
+
+require('./server/config/mongoose.js');
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/client/static"))
+app.use('/node_modules', express.static(__dirname + '/node_modules/'));
+
+
+
+var routes_setter = require('./server/config/routes.js');
+routes_setter(app);
+
+app.listen(8000,function(){
+  console.log('Mean Belt on port 8000')
+})
